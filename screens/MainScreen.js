761a84b3283041511f6f2633b2dfa21464938025
Bryan <morganthemosaic@gmail.com>
2024-07-21 13:45:27 -0400

    Implemented text input for book titles

--- screens/MainScreen.js
+++ screens/MainScreen.js
@@ -1,10 +1,28 @@
-import React from "react";
-import { StyleSheet } from "react-native";
+import React, { useState } from "react";
+import { StyleSheet, TextInput } from "react-native";
 
 const MainScreen = () => {
+	const [title, setTitle] = useState("");
+
+	return (
+		<TextInput
+			autoCapitalize="words"
+			onChangeText={(newTitle) => setTitle(newTitle)}
+			style={styles.textInputStyle}
+			value={title}
+		></TextInput>
+	);
 };
 
 const styles = StyleSheet.create({
+	textInputStyle: {
+		borderBlockColor: "black",
+		borderRadius: 5,
+		borderWidth: 2,
+		fontSize: 18,
+		height: 48,
+		paddingHorizontal: 8
+	}
 });
 
 export default MainScreen;
