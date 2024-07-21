30b3e3dbddce6cefb61ad28b739a6d53dc368c78
Bryan <morganthemosaic@gmail.com>
2024-07-21 14:02:51 -0400

    Added button to add books to list

--- screens/MainScreen.js
+++ screens/MainScreen.js
@@ -1,26 +1,50 @@
 import React, { useState } from "react";
-import { StyleSheet, TextInput } from "react-native";
+import { StyleSheet, TextInput, TouchableOpacity, View } from "react-native";
 
 const MainScreen = () => {
 	const [title, setTitle] = useState("");
+	const [books, setBooks] = useState([]);
 
 	return (
-		<TextInput
-			autoCapitalize="words"
-			onChangeText={(newTitle) => setTitle(newTitle)}
-			style={styles.textInputStyle}
-			value={title}
-		></TextInput>
+		<View style={styles.inputAndButtonContainerStyle}>
+			<TextInput
+				autoCapitalize="words"
+				onChangeText={(newTitle) => setTitle(newTitle)}
+				style={styles.textInputStyle}
+				value={title}
+			/>
+			<TouchableOpacity
+				onPress={() => {
+					setBooks([...books, { title: title }]);
+				}}
+				style={styles.addBooksButtonStyle}
+			></TouchableOpacity>
+		</View>
 	);
 };
 
 const styles = StyleSheet.create({
+	addBooksButtonStyle: {
+		alignItems: "center",
+		backgroundColor: "green",
+		borderRadius: 50,
+		height: 48,
+		justifyContent: "center",
+		width: 48
+	},
+	inputAndButtonContainerStyle: {
+		flexDirection: "row",
+		justifyContent: "space-around",
+		padding: 8
+	},
 	textInputStyle: {
 		borderBlockColor: "black",
 		borderRadius: 5,
 		borderWidth: 2,
+		flex: 1,
 		fontSize: 18,
 		height: 48,
+		marginHorizontal: 8,
 		paddingHorizontal: 8
 	}
 });
