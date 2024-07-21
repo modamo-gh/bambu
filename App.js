bbc7c7974fcb29d67f206f8e5c4cc039a446c3fb
Bryan <morganthemosaic@gmail.com>
2024-07-21 13:35:25 -0400

    Added MainScreen component

--- App.js
+++ App.js
@@ -1,20 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import MainScreen from "./screens/MainScreen";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const Stack = createStackNavigator();
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const App = () => {
+	return (
+		<NavigationContainer>
+			<Stack.Navigator initialRouteName="Main">
+				<Stack.Screen
+					name="Main"
+					component={MainScreen}
+					options={{ title: "bamboo" }}
+				/>
+			</Stack.Navigator>
+		</NavigationContainer>
+	);
+};
+
+export default App;
---
+++ screens/MainScreen.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { StyleSheet } from "react-native";
+
+const MainScreen = () => {
+};
+
+const styles = StyleSheet.create({
+});
+
+export default MainScreen;
