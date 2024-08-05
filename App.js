1da6d35b61b563dd47c696dc5b184ca82f7a5df6
Bryan <morganthemosaic@gmail.com>
2024-08-05 18:03:21 -0400

    Prepare app for distribution

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import { ActionSheetProvider } from "@expo/react-native-action-sheet";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import * as SplashScreenAPI from "expo-splash-screen";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import BookScreen from "./src/screens/BookScreen";
 import MainScreen from "./src/screens/MainScreen";
 import SplashScreen from "./src/screens/SplashScreen";
@@ -12,53 +12,53 @@ SplashScreenAPI.preventAutoHideAsync();
 const Stack = createStackNavigator();
 
 const App = () => {
-	const [isSplashFinished, setIsSplashFinished] = useState(false);
+  const [isSplashFinished, setIsSplashFinished] = useState(false);
 
-	if (!isSplashFinished) {
-		{
-			SplashScreenAPI.hideAsync();
-		}
-		return <SplashScreen setIsSplashFinished={setIsSplashFinished} />;
-	}
+  if (!isSplashFinished) {
+    {
+      SplashScreenAPI.hideAsync();
+    }
+    return <SplashScreen setIsSplashFinished={setIsSplashFinished} />;
+  }
 
-	return (
-		<ActionSheetProvider>
-			<NavigationContainer>
-				<Stack.Navigator initialRouteName="Main">
-					<Stack.Screen
-						name="Main"
-						component={MainScreen}
-						options={{
-							headerStyle: { backgroundColor: "#FEFAE0" },
-							headerTintColor: "#283618",
-							headerTitleStyle: { fontWeight: "bold" },
-							title: "bamboo"
-						}}
-					/>
-					<Stack.Screen
-						name="Book"
-						component={BookScreen}
-						options={{
-							headerStyle: { backgroundColor: "#FEFAE0" },
-							headerTintColor: "#283618",
-							headerTitleStyle: { fontWeight: "bold" },
-							title: "bamboo"
-						}}
-					/>
-					<Stack.Screen
-						name="Splash"
-						component={SplashScreen}
-						options={{
-							headerStyle: { backgroundColor: "#FEFAE0" },
-							headerTintColor: "#283618",
-							headerTitleStyle: { fontWeight: "bold" },
-							title: "bamboo"
-						}}
-					/>
-				</Stack.Navigator>
-			</NavigationContainer>
-		</ActionSheetProvider>
-	);
+  return (
+    <ActionSheetProvider>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Main">
+          <Stack.Screen
+            name="Main"
+            component={MainScreen}
+            options={{
+              headerStyle: { backgroundColor: "#FEFAE0" },
+              headerTintColor: "#283618",
+              headerTitleStyle: { fontWeight: "bold" },
+              title: "bamboo",
+            }}
+          />
+          <Stack.Screen
+            name="Book"
+            component={BookScreen}
+            options={{
+              headerStyle: { backgroundColor: "#FEFAE0" },
+              headerTintColor: "#283618",
+              headerTitleStyle: { fontWeight: "bold" },
+              title: "bamboo",
+            }}
+          />
+          <Stack.Screen
+            name="Splash"
+            component={SplashScreen}
+            options={{
+              headerStyle: { backgroundColor: "#FEFAE0" },
+              headerTintColor: "#283618",
+              headerTitleStyle: { fontWeight: "bold" },
+              title: "bamboo",
+            }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </ActionSheetProvider>
+  );
 };
 
 export default App;
--- metro.config.js
+++ metro.config.js
@@ -1,11 +1,11 @@
-const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
+const { getDefaultConfig } = require('@expo/metro-config');
 
-/**
- * Metro configuration
- * https://reactnative.dev/docs/metro
- *
- * @type {import('metro-config').MetroConfig}
- */
-const config = {};
+const defaultConfig = getDefaultConfig(__dirname);
 
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+module.exports = {
+  ...defaultConfig,
+  resolver: {
+    assetExts: [...defaultConfig.resolver.assetExts, 'svg'],
+    sourceExts: [...defaultConfig.resolver.sourceExts, 'svg', 'json']
+  }
+};
